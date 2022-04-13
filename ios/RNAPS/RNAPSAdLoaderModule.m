aa5017963d87b639a94e066c5e4cd09f1458e25c
Jay Kim <me@wjay.kim>
2022-04-14 04:54:21 +0900

    style: add native code formatter

--- ios/RNAPS/RNAPSAdLoaderModule.m
+++ ios/RNAPS/RNAPSAdLoaderModule.m
@@ -7,10 +7,11 @@
 
 #import <React/RCTBridgeModule.h>
 
-@interface RCT_EXTERN_MODULE(RNAPSAdLoaderModule, NSObject)
+@interface RCT_EXTERN_MODULE (RNAPSAdLoaderModule, NSObject)
 
-RCT_EXTERN_METHOD(loadAd:(NSDictionary)options
-                  withResolver:(RCTPromiseResolveBlock)resolve
-                  withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(loadAd
+                  : (NSDictionary)options withResolver
+                  : (RCTPromiseResolveBlock)resolve withRejecter
+                  : (RCTPromiseRejectBlock)reject)
 
 @end
