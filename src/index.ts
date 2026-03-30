b90915c1f32198a80594833e333e76e67b49e871
Jerome Bonfort <jb@adversport.com>
2026-03-30 15:19:27 +0200

    chore: update license from LGPL-3.0 to MPL-2.0
    - Replaced the LGPL-3.0 license with the Mozilla Public License 2.0 in LICENSE, package.json, and README.md.
    - Updated copyright notices in source files to reflect the new license terms.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,9 @@
 /*
  * Copyright (c) 2022-present Adversport & Contributors
  *
- * This file is part of react-native-aps.
- *
- * react-native-aps is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, version 3 of the License.
- *
- * react-native-aps is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
 export * from './AdLoader';
